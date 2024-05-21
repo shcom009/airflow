118a90507f377e83a48a05c329f38a48e38d8a89
shcom009 <toikki009@gmail.com>
2024-05-21 16:07:54 +0900

    0521

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -23,7 +23,7 @@ with DAG(
 
     @task(task_id='python_xcom_pull_2')
     def xcom_pull_2(status, **kwargs):
-        print('함수 입력값으로 받은 값:' + status)
+        print('함수 입력값으로 받은 값  :' + status)
 
 
     python_xcom_push_by_return = xcom_push_result()
